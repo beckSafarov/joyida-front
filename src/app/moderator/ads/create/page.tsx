d8baf73a07677e0a1f33d7ca33d7dc9e4594daf5
Begzod Safarov <begzod.safarov07@gmail.com>
2024-09-02 07:48:08 +0500

    removed imorting unavailable screens

--- src/app/moderator/ads/create/page.tsx
+++ src/app/moderator/ads/create/page.tsx
@@ -1,6 +1,7 @@
-import CreateAdScreen from '@/modules/Moderator/Ads/CreateAdScreen'
+// import CreateAdScreen from '@/modules/Moderator/Ads/CreateAdScreen'
 import React from 'react'
 
-const CreateAdPage = () => <CreateAdScreen />
+// const CreateAdPage = () => <CreateAdScreen />
+const CreateAdPage = () => <>Page is unavailable</>
 
 export default CreateAdPage
