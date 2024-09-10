178e13604e7fd582a60189ee6c41e53f3af5ddf3
Begzod Safarov <begzod.safarov07@gmail.com>
2024-09-10 14:55:34 +0500

    reels modal bug fix

--- src/modules/Moderator/Reels/components/ReelsModal.tsx
+++ src/modules/Moderator/Reels/components/ReelsModal.tsx
@@ -1,33 +1,31 @@
 import ModalBase from '@/modules/common/Modals/ModalBase'
 import Row from '@/modules/common/Row'
-import { ReelsViewProps } from '@/interfaces/Reels'
 import { Button, Typography } from '@mui/material'
 import React from 'react'
-import ReactPlayer from 'react-player'
 import CheckIcon from '@mui/icons-material/Check'
 import ClearIcon from '@mui/icons-material/Clear'
 import { NewAdminModalProps } from '@/interfaces/superadmin'
 
-const video: ReelsViewProps = {
-  id: '1',
-  thumbnail: '',
-  title: 'Professional ishlash',
-  date: new Date(),
-  views: 10,
-  status: false,
-  source: 'https://youtu.be/86cpfsP0aPs',
-}
+// const video: ReelsViewProps = {
+//   id: '1',
+//   thumbnail: '',
+//   title: 'Professional ishlash',
+//   date: new Date(),
+//   views: 10,
+//   status: false,
+//   source: 'https://youtu.be/86cpfsP0aPs',
+// }
 
 const ReelsModal = (props: NewAdminModalProps) => {
   return (
     <ModalBase
       width='fit-content'
       {...props}
-      title={video.title}
+      title={'sample video'}
       titleAlign='left'
       top='5%'
     >
-      <ReactPlayer url={video.source} controls />
+      {/* <ReactPlayer url={video.source} controls /> */}
       <Row sx={{ width: '100%', mt: '30px', gap: '20px' }}>
         <Button sx={{ width: '50%' }} variant='contained' color='info'>
           <Row spacing={1}>
